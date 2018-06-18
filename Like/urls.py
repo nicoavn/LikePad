69e17441244d6c828a99ff00e03ee5ee2a0715dd
esalas <esalas@encorerepair.com>
2018-06-18 13:28:44 -0400

    Signup

--- Like/urls.py
+++ Like/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view),
+    path('login', views.login_api),
+    path('signup', views.signup_api),
+    path('signup/view', views.signup_view),
     path('like', views.like),
     path('home', views.home),
 ]
--- Like/views.py
+++ Like/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from Like.models import Like
@@ -9,13 +9,41 @@ from Like.Exceptions import DailyVotesAlreadyGivenException, IllegalLikeExceptio
 
 
 def login_view(request):
-    user = request.POST.get('user','')
-    password = request.POST.get('password','')
-    user = authenticate(username=user, password=password)
-    if user:
-        login(request, user)
-        return render(request, "dashboard.html",{})
     return render(request, "login.html",{})
+
+def signup_view(request):
+    return render(request, "signup.html",{})
+
+def login_api(request):
+    user = request.POST.get('user', '')
+    password = request.POST.get('password', '')
+    user_found = authenticate(username=user, password=password)
+
+    if user_found:
+        login(request, user_found)
+        return redirect(home)
+    else:
+        return render(request, "login.html",{'error':'Usuario y/o Password invalidos'})
+
+def signup_api(request):
+    password = request.POST.get('password', '')
+    name = request.POST.get('name', '')
+    last_name = request.POST.get('lastName', '')
+    email = request.POST.get('email', '')
+
+    try:
+        user_created = User.objects.create_user(username=email,email=email,first_name=name,last_name=last_name,password=password)
+    except Exception as e :
+        return render(request, "signup.html", {"error":"Datos incorrectos"})
+
+    if user_created:
+        return render(request, "login.html", {})
+
+    return render(request, "signup.html", {})
+
+def signup_view(request):
+    return render(request, "signup.html",{})
+
 
 @login_required(login_url="/")
 def home(request):
